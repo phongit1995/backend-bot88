9542792e1f92987917382a20c018e8d72643f1d7
phongit <phongit1995@gmail.com>
2021-11-22 09:52:48 +0700

    add v1

--- src/modules/code/code.service.ts
+++ src/modules/code/code.service.ts
@@ -17,23 +17,19 @@ export class CodeService {
     let code = await this.codeModel.findOne({ code: id });
     if (!code) {
       throw new HttpException(
-        `Mã phần mềm không tồn tại 
-        Vui lòng liên hệ Hotline/Zalo : 0977144444 để nhận mã phần mềm !`,
+        `Mã phần mềm không tồn tại. Vui lòng liên hệ Mr. Tiến để nhận mã phần mềm !`,
         HttpStatus.BAD_REQUEST,
       );
     }
     if (!code.type) {
       throw new HttpException(
-        `Lỗi !!!!
-        Điện thoại của bạn không phù hợp mã phần mềm này 
-        Vui lòng nhập mã phần mềm khác`,
+        `Lỗi !!!! Điện thoại của bạn không phù hợp mã phần mềm này. Vui lòng nhập mã phần mềm khác`,
         HttpStatus.BAD_REQUEST,
       );
     }
     if (!code.actived) {
       throw new HttpException(
-        `Mã phần mềm chưa kích hoạt ! 
-        Liên Hệ Hotline/Zalo : 0977147777 Để kích hoạt !`,
+        `Mã phần mềm chưa kích hoạt! Liên Hệ Mr. Tiến Để kích hoạt !`,
         HttpStatus.BAD_REQUEST,
       );
     }
