996c04471db7c993aa47c73c88742685207756a5
phong <phongit1995@gmail.com>
2024-01-10 23:43:57 +0700

    update fix message

--- src/modules/zalo-api-web/zalo-api-web.service.ts
+++ src/modules/zalo-api-web/zalo-api-web.service.ts
@@ -43,7 +43,7 @@ export class ZaloApiWebService {
     if (!code) {
       throw new HttpException(
         `Mã phần mềm không tồn tại 
-        Vui lòng liên hệ Hotline/Zalo : 0979.51.7777 để nhận mã phần mềm !`,
+        Vui lòng liên hệ Admin để nhận mã phần mềm !`,
         HttpStatus.NOT_FOUND,
       );
     }
