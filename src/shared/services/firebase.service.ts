428ac45043cc54eb0c7d8edd46cbef23fe327e30
phong <phongit1995@gmail.com>
2024-11-16 07:30:19 +0700

    update new notification

--- src/shared/services/firebase.service.ts
+++ src/shared/services/firebase.service.ts
@@ -1,20 +1,82 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as fcmPush from 'fcm-push';
+import * as firebaseConfig from './firebase.json';
+import axios from 'axios';
+
+import { JWT } from 'google-auth-library';
+
 @Injectable()
 export class FcmPushService {
   private fcm: any;
+  private readonly fcmUrl = `https://fcm.googleapis.com/v1/projects/${firebaseConfig.project_id}/messages:send`;
   constructor(private configService: ConfigService) {
     this.fcm = new fcmPush(configService.get<string>('NOTIFICATION_KEY'));
   }
   async sendMessage(message: pushMessage): Promise<void> {
-    this.fcm.send(message, function(err, response) {
-      if (err) {
-        console.log(err);
-        console.log('Push Notification False');
-      } else {
-        console.log(response);
-      }
+    const messageToken = {
+      message: {
+        tokens: message.registration_ids,
+        token: message.to,
+        notification: {
+          title: message.notification.title,
+          body: message.notification.body,
+        },
+        android: {
+          ttl: '3600s', 
+          priority: 'high',
+        },
+        apns: {
+          headers: {
+            'apns-priority': '10',
+          },
+        },
+      },
+    };
+    console.log(messageToken);
+    try {
+      // Call Firebase Cloud Messaging API
+      const response = await axios.post(this.fcmUrl, messageToken, {
+        headers: {
+          'Authorization': `Bearer ${await this.getAccessToken()}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log('Notification sent successfully:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error sending notification:', error.response ? error.response.data : error.message);
+      console.log(JSON.stringify(error.response.data))
+      throw error;
+    }
+    // this.fcm.send(message, function(err, response) {
+    //   if (err) {
+    //     console.log(err);
+    //     console.log('Push Notification False');
+    //   } else {
+    //     console.log(response);
+    //   }
+    // });
+  }
+
+  async getAccessToken(){
+    return new Promise(function(resolve, reject) {
+      const jwtClient = new JWT(
+        firebaseConfig.client_email,
+        null,
+        firebaseConfig.private_key,
+        ['https://www.googleapis.com/auth/firebase.messaging'],
+        null
+      );
+      jwtClient.authorize(function(err, tokens) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        console.log(tokens.access_token);
+        resolve(tokens.access_token);
+      });
     });
   }
 }
